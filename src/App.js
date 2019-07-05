a3f99d6e1e5218b3a9efb7f29cb7384837fbf172
Suchith Hegde <suchith.hegde@perficient.com>
2019-07-05 16:37:33 -0400

    chapter11

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
 import EventsPage from './pages/Events';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 import './App.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Redirect from="/" to="/auth" exact />
-        <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={EventsPage} />
-        <Route path="/bookings" component={BookingsPage} />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+          return (
+            <BrowserRouter>
+              <React.Fragment>
+                <MainNavigation />
+                <main>
+                    <Switch>
+                      <Redirect from="/" to="/auth" exact />
+                      <Route path="/auth" component={AuthPage} />
+                      <Route path="/events" component={EventsPage} />
+                      <Route path="/bookings" component={BookingsPage} />
+                    </Switch>
+                </main>
+              </React.Fragment>
+            </BrowserRouter>
+          );
+        }
 }
 
 export default App;
