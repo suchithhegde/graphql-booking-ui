9a628049ad368e6fc5aa44701d86022053c6670d
Suchith Hegde <suchith.hegde@perficient.com>
2019-07-05 21:15:45 -0400

    Chapter13-1143

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
+import './Auth.css';
 
 class AuthPage extends Component {
+  constructor(props) {
+    super(props);
+    this.emailEl = React.createRef();
+    this.passwordEl = React.createRef();
+  }
+
+  submitHandler = () => {
+
+  };
   render() {
-    return (<h1> The Auth Page</h1>);
+    return (
+      <form className="auth-form">
+        <div className="form-control">
+          <label htmlfor="email">E-Mail</label>
+          <input type="email" id="email" />
+        </div>
+        <div className="form-control">
+          <label htmlfor="password">Password</label>
+          <input type="password" id="password" />
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button">Switch to Sign-up</button>
+        </div>
+    </form>
+  );
   }
 }
 
