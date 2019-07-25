14bed8e5c34c299837ff36d1b3f9af8029ae33f8
Suchith Hegde <suchith.hegde@perficient.com>
2019-07-25 17:37:52 -0400

    Chapter 15 PreModal

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 
+import './Events.css';
+
 class EventsPage extends Component {
   render() {
-    return (<h1> The Events Page</h1>);
+    return (
+      <div className="events-control">
+        <p>Please share your Events Information!!</p>
+        <button className="btn">Create Event</button>
+      </div>
+    );
   }
 }
 
